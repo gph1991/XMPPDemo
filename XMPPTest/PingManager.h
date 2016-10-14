05ced1df533cbba587afe5e20814f4c49ab137a7
guopenghao <guopenghao@haogw.com>
2016-10-14 19:44:11 +0800

    single

---
+++ XMPPTest/PingManager.h
@@ -0,0 +1,27 @@
+//
+//  PingManager.h
+//  XMPPTest
+//
+//  Created by Apple on 16/10/14.
+//  Copyright © 2016年 waiguo. All rights reserved.
+//
+
+enum
+{
+    SocketOfflineByServer,// 服务器掉线，默认为0
+    SocketOfflineByUser,  // 用户主动cut
+};
+
+#import <Foundation/Foundation.h>
+
+@interface PingManager : NSObject<XMPPStreamDelegate,XMPPRosterDelegate>
+
+@property (nonatomic, strong) AsyncSocket    *socket;       // socket
+@property (nonatomic, copy  ) NSString       *socketHost;   // socket的Host
+@property (nonatomic, assign) UInt16         socketPort;    // socket的prot
+@property (nonatomic, retain) NSTimer        *connectTimer; // 计时器
+
++(PingManager*)sharedInstance;
+- (void)socketConnectHost;// socket连接
+- (void)cutOffSocket; // 断开socket连接
+@end
